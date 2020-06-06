e4af399539fc9738862d0a9ebe2090e9b4c0929a
Cool BRG <budhram.gurung01@gmail.com>
2020-06-06 08:23:04 +0530

    Edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    if @post.save
+      flash[:notice] = 'Post was updated successfully.'
+      redirect_to @post
+    else
+      flash[:alert] = 'Something went wrong.'
+      render 'edit'
+    end
+  end
+
   private
 
   def post_params
