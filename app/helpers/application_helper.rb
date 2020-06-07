e1f92627ec23cec498e3dc01495ab90cba678049
Cool BRG <budhram.gurung01@gmail.com>
2020-06-07 23:05:55 +0530

    Gravatar and user's show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email = user.email.downcase
+    hash = Digest::MD5.hexdigest(email)
+    size = options[:size]
+    image_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+
+    image_tag(image_url, alt: user.username, class: "shadow-sm rounded-circle mx-auto d-block")
+  end
 end
