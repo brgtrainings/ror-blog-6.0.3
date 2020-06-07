21315c784754c62cf1fa42d7880bfe00948c8035
Cool BRG <budhram.gurung01@gmail.com>
2020-06-07 21:47:11 +0530

    Added bcrypt for secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_124230) do
+ActiveRecord::Schema.define(version: 2020_06_07_161050) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_124230) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
