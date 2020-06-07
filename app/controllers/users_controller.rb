685e2122cdfd57bbae8d962f91597f396943cd4e
Cool BRG <budhram.gurung01@gmail.com>
2020-06-07 22:18:56 +0530

    User sign up form and associated controller actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to BRG Blog #{@user.username}, you have successfully sign up."
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Failed to create user.'
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :posts
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
