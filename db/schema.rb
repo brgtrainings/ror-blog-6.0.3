3c7f9d0e87cfe3033318259887d1d17944c909f7
Cool BRG <budhram.gurung01@gmail.com>
2020-06-07 18:01:57 +0530

    rails g model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_145247) do
+ActiveRecord::Schema.define(version: 2020_06_07_123012) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
