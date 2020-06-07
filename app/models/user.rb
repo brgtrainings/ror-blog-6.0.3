79f9cd7b4c0e9d79c0e76760bb3a4bf4e3df9447
Cool BRG <budhram.gurung01@gmail.com>
2020-06-07 18:12:06 +0530

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 4, maximum: 20 }
+  validates :email, presence: true,
+                    uniqueness:  { case_sensitive: false },
+                    length: { maximum: 120 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
