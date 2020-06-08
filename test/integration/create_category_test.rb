a2dcbff661ecc1bdeaf760246980a00668953a14
Cool BRG <budhram.gurung01@gmail.com>
2020-06-08 23:33:44 +0530

    Integration negative flow test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test 'get new category and create category' do
+  test 'get new category form and create category' do
     get '/categories/new'
 
     assert_response :success
@@ -12,5 +12,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match 'Travel', response.body
+  end
+
+  test 'get new category from and reject invalid category submission' do
+    get '/categories/new'
+
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_url, params: { category: { name: '' } }
+    end
+
+    assert_match 'errors', response.body
+    assert_select 'div.alert'
+    assert_select 'ul li'
   end
 end
