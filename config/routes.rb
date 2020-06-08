5b136a6e09e7ae98ed5d5b233cfac4da93ac8301
Cool BRG <budhram.gurung01@gmail.com>
2020-06-08 12:05:20 +0530

    Add login, logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   resources :posts
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
 end
