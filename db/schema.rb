2b692e89a2b70b207317ea4b26c357ff46d1c590
Cool BRG <budhram.gurung01@gmail.com>
2020-06-08 16:09:45 +0530

    Add admin migration to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_161050) do
+ActiveRecord::Schema.define(version: 2020_06_08_103544) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_161050) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
