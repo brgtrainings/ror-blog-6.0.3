22a98f8e228cc6cd50ddf791aeb91edf1758c13e
Cool BRG <budhram.gurung01@gmail.com>
2020-06-09 00:11:09 +0530

    Require admin for category and fix test as sign_in_as helper

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin_user = User.create(username: 'admin', email: 'admin@test.com',
+                              password: 'password', admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test 'get new category form and create category' do
     get '/categories/new'
 
