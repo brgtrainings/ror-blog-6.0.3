24bc5023cac3ec94cc7c6b139f7f6d64da52b7fc
Cool BRG <budhram.gurung01@gmail.com>
2020-06-09 17:27:21 +0530

    Category index and show, updated posts and users too

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @posts = @category.posts.paginate(page: params[:page], per_page: 3)
   end
 
   def new
